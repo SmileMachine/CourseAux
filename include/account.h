88db9473cdead09c537580314f809e3a64daa16c
nace <72872637+kzzssn@users.noreply.github.com>
2022-04-14 20:51:51 +0800

    delete

--- include/account.h
+++ include/account.h
@@ -3,8 +3,10 @@
 #include <cstdio>
 #include <cstring>
 #include "data_io.h"
-struct UserInfo {
-	enum Status {
+struct UserInfo
+{
+	enum Status
+	{
 		student,
 		teacher,
 		admin,
@@ -15,10 +17,11 @@ struct UserInfo {
 	char group[15] = {}; //用户所在的群组，如班级
 	Status status;		 //该用户的身份
 };
-struct Password { //用户口令相关
+struct Password
+{ //用户口令相关
 public:
-	bool input();			//输入密码,并检查是否匹配
-	bool set(char str[]);	//设置密码
+	bool input();		  //输入密码,并检查是否匹配
+	bool set(char str[]); //设置密码
 	typedef unsigned int DataType;
 	static DataType hash(const char str[]);
 	//将字符串映射为32位无符号整数,此处使用BKDR_Hash
@@ -28,19 +31,21 @@ public:
 private:
 };
 
-class Account {
+class Account
+{
 public:
 	UserInfo profile;  //用户信息
 	Password password; //用户密码
 	void printInfo();
 };
 
-class AccountList {
+class AccountList
+{
 public:
-	AccountList(const char path[]); //Todo:采用更佳的数据结构，方便查找
+	AccountList(const char path[]); // Todo:采用更佳的数据结构，方便查找
 	bool init(const char path[]);
-	Account* findByID(char id[]);	//通过ID查找
-#define AL_SIZE 100					//初步定为100
+	Account *findByID(char id[]); //通过ID查找
+#define AL_SIZE 100				  //初步定为100
 	Account list[AL_SIZE];
 
 private:
@@ -48,5 +53,5 @@ private:
 };
 
 extern AccountList accountList;
-extern Account* currentAccount;
+extern Account *currentAccount;
 #endif
